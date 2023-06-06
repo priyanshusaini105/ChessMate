675e0fdbc0daa760c4f0f22b72b6647eed14b1ff
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-06-07 02:53:46 +0530

    Create GridManager Elemnts

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -4,5 +4,20 @@ using UnityEngine;
 
 public class GridManager : MonoBehaviour
 {
-    
+    [SerializeField] private int _width,_height;
+    [SerializeField] private Tile _tilePrefab;
+
+    void Start()
+    {
+        GenerateGrid();
+    }
+
+    void GenerateGrid(){
+        for(int x=0;x<_width;x++){
+            for(int y=0;y<_height;y++){
+                var spawnedTile=Instantiate(_tilePrefab,new Vector3(x,y),Quaternion.identity); // Instantiate the tile
+                spawnedTile.name="Tile "+x+" "+y; // Set the name of the tile
+            }
+        }
+    }
 }
