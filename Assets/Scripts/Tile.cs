268ee58ca48e2d87f5b7a9e19b4265ed47dd2a6c
Piyanshu Saini <priyanshusaininew@gmail.com>
2023-06-07 16:19:35 +0530

    Create script for tile

--- Assets/Scripts/Tile.cs
+++ Assets/Scripts/Tile.cs
@@ -1,18 +1,22 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
-public class Tile : MonoBehaviour
-{
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+ 
+public class Tile : MonoBehaviour {
+    [SerializeField] private Color _baseColor, _offsetColor;
+    [SerializeField] private SpriteRenderer _renderer;
+    [SerializeField] private GameObject _highlight;
+ 
+    public void Init(bool isOffset) {
+        _renderer.color = isOffset ? _offsetColor : _baseColor;
     }
-
-    // Update is called once per frame
-    void Update()
+ 
+    void OnMouseEnter() {
+        _highlight.SetActive(true);
+    }
+ 
+    void OnMouseExit()
     {
-        
+        _highlight.SetActive(false);
     }
 }
